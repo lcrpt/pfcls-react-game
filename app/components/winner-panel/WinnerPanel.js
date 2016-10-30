c54006026f78b6e98e142460bb1e9179b5dd651f
leolassence <leolassence@gmail.com>
2016-10-30 12:09:42 +0100

    updated winner panel component for a better DRY

--- app/components/winner-panel/WinnerPanel.js
+++ app/components/winner-panel/WinnerPanel.js
@@ -2,41 +2,30 @@ import React from 'react';
 
 class WinnerPanel extends React.Component {
   render() {
+    let cardTitle = '';
+    
     if (this.props.winner.isWinner) {
-      return (
-        <div className="col-xs-12">
-          <div className="card card-pricing">
-            <div className="content content-primary">
-              <h3 className="card-title">The winner is {this.props.winner.winner.name}</h3>
-              <h3 className="card-title">Score:</h3>
-              <ul>
-                <li>{this.props.firstPlayer.name} <b>{this.props.firstPlayer.score}</b></li>
-                <li>{this.props.secondPlayer.name} <b>{this.props.secondPlayer.score}</b></li>
-              </ul>
-              <a href="#" className="btn btn-white btn-round">Next round</a>
-              <br /><br />
-            </div>
-          </div>
-        </div>
-      );
+      cardTitle = `The winner is ${this.props.winner.winner.name}`;
     } else {
-      return (
-        <div className="col-xs-12">
-          <div className="card card-pricing">
-            <div className="content content-primary">
-              <h3 className="card-title">Nobody won this Round</h3>
-              <h3 className="card-title">Score:</h3>
-              <ul>
-                <li>{this.props.firstPlayer.name} <b>{this.props.firstPlayer.score}</b></li>
-                <li>{this.props.secondPlayer.name} <b>{this.props.secondPlayer.score}</b></li>
-              </ul>
-              <a href="#" className="btn btn-white btn-round">Next round</a>
-              <br /><br />
-            </div>
+      cardTitle = 'Nobody won this Round';
+    }
+
+    return (
+      <div className="col-xs-12">
+        <div className="card card-pricing">
+          <div className="content content-primary">
+            <h3 className="card-title">{cardTitle}</h3>
+            <h3 className="card-title">Score:</h3>
+            <ul>
+              <li>{this.props.firstPlayer.name} <b>{this.props.firstPlayer.score}</b></li>
+              <li>{this.props.secondPlayer.name} <b>{this.props.secondPlayer.score}</b></li>
+            </ul>
+            <a href="#" className="btn btn-white btn-round">Next round</a>
+            <br /><br />
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
@@ -51,6 +40,9 @@ WinnerPanel.propTypes = {
   }),
   winner: React.PropTypes.shape({
     name: React.PropTypes.string,
+    winner: React.PropTypes.shape({
+      name: React.PropTypes.string,
+    }),
     isWinner: React.PropTypes.bool.isRequired,
   }),
 };
