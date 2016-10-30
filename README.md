908945b01a854fb6713e499390ddab024be1618b
leolassence <leolassence@gmail.com>
2016-10-30 23:00:12 +0100

    fix package.json syntaxe error

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # PFCLS game realized with React and Webpack
 
 **It includes:**
-
 - [x] React
 - [x] es6
 - [x] React Router
 - [x] Webpack Dev Server
 - [x] Hot Code Reloading
-- [x] SASS
+- [x] sass
+- [x] babel
 - [x] eslint
 
 ## Starting the dev server
 
 1. `git clone git@github.com:leolassence/PFCLS-react-webpack.git`
-2. Run `npm i`
+2. run `npm i`
 3. Start the dev server using `npm start`
-3. Open [http://localhost:8080](http://localhost:8080)
+3. Open [localhost:8080](http://localhost:8080)
 
 ## Available Commands
 
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,55 +1,64 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
-var config = {
+const config = {
   devtool: 'cheap-module-source-map',
 
   entry: [
     path.resolve(__dirname, 'app/main.js'),
-    path.resolve(__dirname, 'app/assets/scss/main.scss')
+    path.resolve(__dirname, 'app/assets/scss/main.scss'),
   ],
 
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
 
   devServer: {
-    outputPath: path.join(__dirname, 'dist')
+    outputPath: path.join(__dirname, 'dist'),
   },
 
   module: {
     loaders: [
       {
         test: /\.js?$/,
-        include: path.resolve(__dirname, 'app'),
         exclude: /node_modules/,
-        loader: 'babel'
+        include: path.resolve(__dirname, 'app'),
+        loader: 'babel',
+        query: {
+          presets: ['react-hmre'],
+        },
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!sass-loader'})
-      }
-    ]
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader!sass-loader' }),
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=15000',
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+    ],
   },
 
   plugins: [
     HtmlWebpackPluginConfig,
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.optimize.UglifyJsPlugin({ comments: false }),
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new ExtractTextPlugin("style.css", {allChunks: false}),
-    new CopyWebpackPlugin([{ from: 'app/vendors', to: 'vendors' }])
-  ]
+    new ExtractTextPlugin('style.css', { allChunks: false }),
+    new CopyWebpackPlugin([{ from: 'app/vendors', to: 'vendors' }]),
+  ],
 };
 
 module.exports = config;
